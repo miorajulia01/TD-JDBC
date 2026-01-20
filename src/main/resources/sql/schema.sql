018e8730649617c62a61b8dd6ab590384ee2734c
miorajulia01 <miorahanitra01.com>
2026-01-20 17:20:23 +0300

    modification de schema.sql en mew_schema.sql

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -12,14 +12,19 @@ CREATE TYPE dish_type AS ENUM (
     'DESSERT'
 );
 
+CREATE  TYPE unit_type AS ENUM (
+    'PCS',
+    'KG',
+    'L'
+)
+
 CREATE TABLE Dish
 (
-    id int PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    dish_type dish_type NOT NULL
+    dish_type dish_type NOT NULL,
+    price numeric (10, 2)
 );
-
-ALTER TABLE dish ADD COLUMN IF NOT EXISTS price NUMERIC(10, 2);
 
 
 CREATE TABLE Ingredient
@@ -27,10 +32,15 @@ CREATE TABLE Ingredient
     id       int PRIMARY KEY,
     name     VARCHAR(100)        NOT NULL,
     price    NUMERIC(6, 2)       NOT NULL,
-    category ingredient_category NOT NULL,
-    id_dish  INT,
-    CONSTRAINT fk_dish
-    FOREIGN KEY (id_dish)
-    REFERENCES Dish (id)
+    category ingredient_category NOT NULL
+
 );
 
+CREATE TABLE dish_ingredient(
+    id serial PRIMARY KEY ,
+    quantity_required numeric (6, 2) NOT NULL ,
+    id_dish INT NOT NULL REFERENCES Ingredient(id) ON DELETE CASCADE ,
+    id_ingredient INT NOT NULL REFERENCES ingredient(id) ON DELETE CASCADE,
+    quantity_required NUMERIC(6,2) NOT NULL,
+    unit unit_type NOT NULL
+);
